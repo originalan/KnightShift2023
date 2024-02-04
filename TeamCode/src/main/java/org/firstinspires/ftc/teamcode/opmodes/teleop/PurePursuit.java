3b76259a13f3007d3c770fcd72b1b9d4c359e2d7
originalanqu <originalanqu@gmail.com>
2024-02-04 18:20:30 -0500

    smol comit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/PurePursuit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/PurePursuit.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import static org.firstinspires.ftc.teamcode.purepursuit.RobotMovement.followCurve;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -16,6 +18,9 @@ public class PurePursuit extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+        telemetry.addLine("is not functional yet, many bugs");
+        telemetry.update();
 
         waitForStart();
         if (opModeIsActive()) {
